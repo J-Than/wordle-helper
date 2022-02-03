b9f774ac2c3b593c3546c2e296e9d0b3cf5df75f
Jarod Wilson <jaroddwilson@gmail.com>
2022-02-03 15:00:53 -0500

    Built first half of function to store letters

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 // Declare universal variables
+const knownLetters = ['','','','',''];
+const goodLetters = [];
+const badLetters = [];
 
 // Add event listeners
 document.getElementById('letter-state').addEventListener('submit', (e) => {
   e.preventDefault();
-  submitHandler();
+  storeLetters();
 })
 
 // Handle form submission
-function submitHandler() {
-  console.log('Clicked on the submission button!');
+function storeLetters() {
+  knownLetters[0] = document.getElementById('first-letter').value.toLowerCase();
+  knownLetters[1] = document.getElementById('second-letter').value.toLowerCase();
+  knownLetters[2] = document.getElementById('third-letter').value.toLowerCase();
+  knownLetters[3] = document.getElementById('fourth-letter').value.toLowerCase();
+  knownLetters[4] = document.getElementById('fifth-letter').value.toLowerCase();
 }
