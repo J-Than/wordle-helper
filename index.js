d6b38a7d9a9992b238518b7c01d980968dc89c08
Jarod Wilson <jaroddwilson@gmail.com>
2022-02-03 15:09:11 -0500

    Finished the letter stroage function

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 // Declare universal variables
-const knownLetters = ['','','','',''];
-const goodLetters = [];
-const badLetters = [];
+let knownLetters = ['','','','',''];
+let goodLetters;
+let badLetters;
 
 // Add event listeners
 document.getElementById('letter-state').addEventListener('submit', (e) => {
   e.preventDefault();
   storeLetters();
+  searchWords();
 })
 
-// Handle form submission
+// Store letters for use in search
 function storeLetters() {
   knownLetters[0] = document.getElementById('first-letter').value.toLowerCase();
   knownLetters[1] = document.getElementById('second-letter').value.toLowerCase();
   knownLetters[2] = document.getElementById('third-letter').value.toLowerCase();
   knownLetters[3] = document.getElementById('fourth-letter').value.toLowerCase();
   knownLetters[4] = document.getElementById('fifth-letter').value.toLowerCase();
+  goodLetters = document.getElementById('yellow-letters').value.toLowerCase().split('');
+  badLetters = document.getElementById('black-letters').value.toLowerCase().split('');
 }
+
+// Search for words that match the given parameters
